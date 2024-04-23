368349a2838ddb9e5195387e9f14c733f61b6942
Thangapandi-K <thangapandi.chella@gmail.com>
2024-04-23 09:24:51 +0530

    Task Submission

---
+++ ResumeJSON.js
@@ -0,0 +1,26 @@
+
+
+
+const myResume = {
+    name : "Thangapandi K",
+    age : 27,
+    address : {
+        city : "Bodinayakanur",
+        district : "Theni",
+        state : "Tamilnadu",
+        country : "India"
+    },
+    contact : {
+        mobile : "+919952257347",
+        email : "thangapandi.chella@gmail.com"
+    },
+    education : {
+        degree : "Bachelor of Science",
+        year : "2017",
+        college : "Annai fathima College of Arts and Science",
+        university : "Madurai Kamaraj University",
+        location : "Madurai, Tamilnadu"
+    } 
+};
+
+
